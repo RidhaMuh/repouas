4a2dbc98de47e911a37f0f871dae330eb003760f
RidhaMuh <fzridhaa@gmail.com>
2024-07-10 15:33:45 +0800

    Penambahan fitur hasil: NIM dan Kelas

--- uas.cpp
+++ uas.cpp
@@ -3,6 +3,8 @@ using namespace std;
 
 int main() {
   cout << "UAS STRUKTUR DATA!";
-  cout << "Nama : M.Ridha Maulana";
+  cout << "Nama  : M.Ridha Maulana";
+  cout << "NIM   : C030323037";
+  cout << "Kelas : TI 2A";
   return 0;
 }
