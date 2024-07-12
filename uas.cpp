12c28e799c62a1357d880d79316346790a26a745
RidhaMuh <fzridhaa@gmail.com>
2024-07-12 16:57:45 +0800

    Memasukkan struct untuk menyimpan data barang di gudang

--- uas.cpp
+++ uas.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 
 int main() {
-  cout << "UAS STRUKTUR DATA!"/n;
-  cout << "Nama  : M.Ridha Maulana"/n;
-  cout << "NIM   : C030323037"/n;
+  cout << "UAS STRUKTUR DATA!\n";
+  cout << "Nama  : M.Ridha Maulana\n";
+  cout << "NIM   : C030323037\n";
   cout << "Kelas : TI 2A";
   return 0;
 }
